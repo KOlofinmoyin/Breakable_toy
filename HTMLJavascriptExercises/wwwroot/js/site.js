4c75ce154398acc8e8e96ab922c9892a904b2f93
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-08-23 17:17:56 +0100

    new toy to play with js function expressions

---
+++ HTMLJavascriptExercises/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+alert("Here is an alert!")
+prompt = function () {
+    return "Hello!";
+}
+var name = prompt("Give me your name", "Enter your name here...")
+alert("Your name is: " + name)
+var clientConfirm = confirm("Do you confirm")
+if (clientConfirm) {
+    alert("Welcome my Prince");
+} else {
+    alert("Wrong password");
+}
